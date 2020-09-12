9a2bac4ddb4c5ebb1795f0fedb1352482c1f33f7
ethanga12 <66858278+ethanga12@users.noreply.github.com>
2020-09-12 09:56:05 -0400

    storing for now

--- popups/content.js
+++ popups/content.js
@@ -1,3 +1,11 @@
-chrome.runtime.onMessage.addListener(function (request) {
-  alert(request)
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   // alert(request)
+//
+//   sendResponse({count: matches.length})
+// })
+const re = new RegExp('this', 'gi')
+const matches = document.documentElement.innerHTML.match(re)
+chrome.runtime.sendMessage({
+  url: window.location.href,
+  count: matches.length 
 })
--- popups/popup.js
+++ popups/popup.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   function onclick () {
     chrome.tabs.query({currentWindow: true, active: true},
     function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, 'yo')
+      chrome.tabs.sendMessage(tabs[0].id, 'yo', setCount)
     })
+  }
+
+  function setCount (res) {
+    const div = document.createElement('div')
+    div.textContent = `${res.count} this`
   }
 } , false)
